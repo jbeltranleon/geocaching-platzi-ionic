496ba79c39c768bc0f939b0e54c3c96b81e2b20c
Jhonbeltran <jbeltranleon@gmail.com>
2018-12-19 16:07:49 -0500

    importo Lugar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LugarPage } from '../pages/lugar/lugar';
 
 @NgModule({
   declarations: [
